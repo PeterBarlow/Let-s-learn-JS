3bcc9a3da9a49c91936aa5f5a82fa11c0d5ca739
PeterBarlow <peterruelbarlow@gmail.com>
2015-09-07 11:25:06 +0200

    2

--- test.js
+++ test.js
@@ -9,7 +9,43 @@ var products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 }
 ];
 
 function compareSold(colaA, colaB) {
-	return colaA - colaB;
+	if (colaA.sold > colaB.sold) {
+		return 1;		
+	} else if (colaA.sold === colaB.sold) {
+		return 0;
+	} else {
+		return -1;
+	}
+};
+
+function compareName(colaA, colaB) {
+	if (colaA.name > colaB.name) {
+		return 1;		
+	} else if (colaA.name === colaB.name) {
+		return 0;
+	} else {
+		return -1;
+	}
+};
+
+function compareCalories(colaA, colaB) {
+	if (colaA.calories > colaB.calories) {
+		return 1;		
+	} else if (colaA.calories === colaB.calories) {
+		return 0;
+	} else {
+		return -1;
+	}
+};
+
+function compareColor(colaA, colaB) {
+	if (colaA.color > colaB.color) {
+		return 1;		
+	} else if (colaA.color === colaB.color) {
+		return 0;
+	} else {
+		return -1;
+	}
 };
 
 products.sort(compareSold);
@@ -24,4 +60,18 @@ function printProducts(products) {
 }
 
 products.sort(compareSold);
+console.log("Products sorted by number sold:");
 printProducts(products);
+
+products.sort(compareName);
+console.log("Products sorted by name:");
+printProducts(products);
+
+products.sort(compareCalories);
+console.log("Products sorted by calories:");
+printProducts(products);
+
+products.sort(compareColor);
+console.log("Products sorted by color:");
+printProducts(products);
+
